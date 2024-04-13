144a9f947573fe0608baf775db6cebe7c874ff2a
DiegoD <doimo.diego@gmail.com>
2024-04-13 20:28:27 +0200

    u

--- cluster_analysis/intrinsic_dimension/compute_distances.py
+++ cluster_analysis/intrinsic_dimension/compute_distances.py
@@ -138,54 +138,58 @@ def compute_id(
             pickle.dump(statistics, f)
 
         act_dict = extr_act.hidden_states
-        for i, (layer, act) in enumerate(act_dict.items()):
-            if save_repr:
+
+        if save_repr:
+            for i, (layer, act) in enumerate(act_dict.items()):
                 torch.save(act, f"{dirpath}/l{target_layer_labels[i]}{filename}.pt")
 
-            act = act.to(torch.float64).numpy()
+        if save_distances:
+            for i, (layer, act) in enumerate(act_dict.items()):
 
-            save_backward_indices = False
-            if remove_duplicates:
-                act, idx, inverse = np.unique(
-                    act, axis=0, return_index=True, return_inverse=True
+                act = act.to(torch.float64).numpy()
+
+                save_backward_indices = False
+                if remove_duplicates:
+                    act, idx, inverse = np.unique(
+                        act, axis=0, return_index=True, return_inverse=True
+                    )
+                    accelerator.print(len(idx), len(inverse))
+                    if len(idx) == len(inverse):
+                        # if no overlapping data has been found return the original ordred array
+                        assert len(np.unique(inverse)) == len(inverse)
+                        act = act[inverse]
+                    else:
+                        save_backward_indices = True
+                    accelerator.print(f"num_duplicates = {len(inverse)-len(idx)}")
+
+                accelerator.print(f"{layer} act_shape {act.shape}")
+                sys.stdout.flush()
+
+                n_samples = act.shape[0]
+                print(n_samples)
+                range_scaling = min(1050, n_samples - 1)
+                maxk = min(maxk, n_samples - 1)
+
+                start = time.time()
+                distances, dist_index, mus, _ = compute_distances(
+                    X=act,
+                    n_neighbors=maxk + 1,
+                    n_jobs=1,
+                    working_memory=2048,
+                    range_scaling=range_scaling,
+                    argsort=False,
                 )
-                accelerator.print(len(idx), len(inverse))
-                if len(idx) == len(inverse):
-                    # if no overlapping data has been found return the original ordred array
-                    assert len(np.unique(inverse)) == len(inverse)
-                    act = act[inverse]
-                else:
-                    save_backward_indices = True
-                accelerator.print(f"num_duplicates = {len(inverse)-len(idx)}")
+                accelerator.print((time.time() - start) / 60, "min")
 
-            accelerator.print(f"{layer} act_shape {act.shape}")
-            sys.stdout.flush()
-
-            n_samples = act.shape[0]
-            print(n_samples)
-            range_scaling = min(1050, n_samples - 1)
-            maxk = min(maxk, n_samples - 1)
-
-            start = time.time()
-            distances, dist_index, mus, _ = compute_distances(
-                X=act,
-                n_neighbors=maxk + 1,
-                n_jobs=1,
-                working_memory=2048,
-                range_scaling=range_scaling,
-                argsort=False,
-            )
-            accelerator.print((time.time() - start) / 60, "min")
-
-            if save_distances:
                 np.save(
                     f"{dirpath}/l{target_layer_labels[i]}{filename}_dist", distances
                 )
                 np.save(
                     f"{dirpath}/l{target_layer_labels[i]}{filename}_index", dist_index
                 )
-            if save_backward_indices:
-                np.save(
-                    f"{dirpath}/l{target_layer_labels[i]}{filename}_inverse", inverse
-                )
-            np.save(f"{dirpath}/l{target_layer_labels[i]}{filename}_mus", mus)
+                if save_backward_indices:
+                    np.save(
+                        f"{dirpath}/l{target_layer_labels[i]}{filename}_inverse",
+                        inverse,
+                    )
+                np.save(f"{dirpath}/l{target_layer_labels[i]}{filename}_mus", mus)
