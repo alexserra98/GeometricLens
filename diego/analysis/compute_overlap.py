f94db0305b976f4336d546d7dac833d9f5ffabb3
DiegoD <doimo.diego@gmail.com>
2024-05-04 21:03:34 +0200

    u

--- diego/analysis/compute_overlap.py
+++ diego/analysis/compute_overlap.py
@@ -118,97 +118,97 @@ for shot_mode in ["Oshot", "5shot"]:
                 stats = pickle.load(f)
             subjects = np.array(stats["subjects"])
 
-        # balance the test set if asked
-        is_balanced = ""
-        if dataset_mask is not None:
-            is_balanced = "_balanced"
-            base_repr = base_repr[dataset_mask]
-            finetuned_repr = finetuned_repr[dataset_mask]
-            subjects = subjects[dataset_mask]
-            # check that all the subjects have the same frequency
-            frequences = Counter(subjects).values()
-            assert len(np.unique(list(frequences))) == 1
-            # check that the frequency is 100
-            assert np.unique(list(frequences))[0] == 100, (
-                np.unique(list(frequences))[0],
-                frequences,
-            )
-
-            # remove identical points
-            base_unique, base_idx, base_inverse = np.unique(
-                base_repr, axis=0, return_index=True, return_inverse=True
-            )
-            finetuned_unique, finetuned_idx, finetuned_inverse = np.unique(
-                finetuned_repr, axis=0, return_index=True, return_inverse=True
-            )
-            indices = np.intersect1d(base_idx, finetuned_idx)
-            indices = np.sort(indices)
-
-            base_repr = base_repr[indices]
-            finetuned_repr = finetuned_repr[indices]
-            subjects = subjects[indices]
-
-            # ***********************************************************************
-
-            maxk = 300
-            assert indices.shape[0] > maxk, (indices.shape[0], maxk)
-            distances_base, dist_index_base, mus, _ = compute_distances(
-                X=base_repr,
-                n_neighbors=maxk + 1,
-                n_jobs=1,
-                working_memory=2048,
-                range_scaling=maxk + 2,
-                argsort=False,
-            )
-
-            d = data.Data(distances=(distances_base, dist_index_base))
-            ids, _, _ = d.return_id_scaling_gride(range_max=100)
-            d.set_id(ids[3])
-            d.compute_density_kNN(k=2**4)
-            assignment_base = d.compute_clustering_ADP(Z=1.6)
-
-            distances_finetuned, dist_index_finetuned, mus, _ = compute_distances(
-                X=finetuned_repr,
-                n_neighbors=maxk + 1,
-                n_jobs=1,
-                working_memory=2048,
-                range_scaling=maxk + 2,
-                argsort=False,
-            )
-
-            d = data.Data(distances=(distances_base, dist_index_base))
-            ids, _, _ = d.return_id_scaling_gride(range_max=100)
-            d.set_id(ids[3])
-            d.compute_density_kNN(k=2**4)
-            assignment_finetuned = d.compute_clustering_ADP(Z=1.6)
-
-            clusters[f"ep{epoch}-ami"].append(
-                adjusted_mutual_info_score(assignment_base, assignment_finetuned)
-            )
-
-            clusters[f"ep{epoch}-ari"].append(
-                adjusted_rand_score(assignment_base, assignment_finetuned)
-            )
-
-            for k in [30, 100, 300]:
-                ov_repr[f"ep{epoch}_k{k}"].append(
-                    return_data_overlap(
-                        indices_base=dist_index_base,
-                        indices_other=dist_index_finetuned,
-                        k=k,
-                        subjects=None,
-                    )
+            # balance the test set if asked
+            is_balanced = ""
+            if dataset_mask is not None:
+                is_balanced = "_balanced"
+                base_repr = base_repr[dataset_mask]
+                finetuned_repr = finetuned_repr[dataset_mask]
+                subjects = subjects[dataset_mask]
+                # check that all the subjects have the same frequency
+                frequences = Counter(subjects).values()
+                assert len(np.unique(list(frequences))) == 1
+                # check that the frequency is 100
+                assert np.unique(list(frequences))[0] == 100, (
+                    np.unique(list(frequences))[0],
+                    frequences,
                 )
 
-            ov_repr_tmp = return_data_overlap(
-                indices_base=dist_index_base,
-                indices_other=dist_index_finetuned,
-                k=30,
-                subjects=subjects,
-            )
+                # remove identical points
+                base_unique, base_idx, base_inverse = np.unique(
+                    base_repr, axis=0, return_index=True, return_inverse=True
+                )
+                finetuned_unique, finetuned_idx, finetuned_inverse = np.unique(
+                    finetuned_repr, axis=0, return_index=True, return_inverse=True
+                )
+                indices = np.intersect1d(base_idx, finetuned_idx)
+                indices = np.sort(indices)
 
-            for subject in np.unique(subjects):
-                ov_repr[f"ep{epoch}_{subject}_k30"].append(ov_repr_tmp[subject])
+                base_repr = base_repr[indices]
+                finetuned_repr = finetuned_repr[indices]
+                subjects = subjects[indices]
+
+                # ***********************************************************************
+
+                maxk = 300
+                assert indices.shape[0] > maxk, (indices.shape[0], maxk)
+                distances_base, dist_index_base, mus, _ = compute_distances(
+                    X=base_repr,
+                    n_neighbors=maxk + 1,
+                    n_jobs=1,
+                    working_memory=2048,
+                    range_scaling=maxk + 2,
+                    argsort=False,
+                )
+
+                d = data.Data(distances=(distances_base, dist_index_base))
+                ids, _, _ = d.return_id_scaling_gride(range_max=100)
+                d.set_id(ids[3])
+                d.compute_density_kNN(k=2**4)
+                assignment_base = d.compute_clustering_ADP(Z=1.6)
+
+                distances_finetuned, dist_index_finetuned, mus, _ = compute_distances(
+                    X=finetuned_repr,
+                    n_neighbors=maxk + 1,
+                    n_jobs=1,
+                    working_memory=2048,
+                    range_scaling=maxk + 2,
+                    argsort=False,
+                )
+
+                d = data.Data(distances=(distances_base, dist_index_base))
+                ids, _, _ = d.return_id_scaling_gride(range_max=100)
+                d.set_id(ids[3])
+                d.compute_density_kNN(k=2**4)
+                assignment_finetuned = d.compute_clustering_ADP(Z=1.6)
+
+                clusters[f"ep{epoch}-ami"].append(
+                    adjusted_mutual_info_score(assignment_base, assignment_finetuned)
+                )
+
+                clusters[f"ep{epoch}-ari"].append(
+                    adjusted_rand_score(assignment_base, assignment_finetuned)
+                )
+
+                for k in [30, 100, 300]:
+                    ov_repr[f"ep{epoch}_k{k}"].append(
+                        return_data_overlap(
+                            indices_base=dist_index_base,
+                            indices_other=dist_index_finetuned,
+                            k=k,
+                            subjects=None,
+                        )
+                    )
+
+                ov_repr_tmp = return_data_overlap(
+                    indices_base=dist_index_base,
+                    indices_other=dist_index_finetuned,
+                    k=30,
+                    subjects=subjects,
+                )
+
+                for subject in np.unique(subjects):
+                    ov_repr[f"ep{epoch}_{subject}_k30"].append(ov_repr_tmp[subject])
 
     with open(
         f"{args.results_path}/overlaps_{args.model}_finetuned_{args.finetuned_mode}_eval_{args.eval_dataset}{is_balanced}_epoch{args.epochs}_{shot_mode}.pkl",
